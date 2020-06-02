b97113d9fca6e63e9d3d85e02ff3f343ce28e67c
Pavlos Saba <pavlossaba@gmail.com>
2020-06-02 12:36:22 +0300

    Fixed some stuff

--- portfolio/app.js
+++ portfolio/app.js
@@ -27,3 +27,20 @@ menu_item.forEach((item) => {
     mobile_menu.classList.toggle("active");
   });
 });
+var modal = document.querySelector(".modal");
+var trigger = document.querySelector(".trigger");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
