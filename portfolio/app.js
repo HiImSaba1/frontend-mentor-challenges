c7b6957297a97d889e93fc6a8889f4af86c2021a
Pavlos Saba <pavlossaba@gmail.com>
2020-06-02 12:48:36 +0300

    Fixed some stuff

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,10 +1,15 @@
-const hamburger = document.querySelector(
-  ".header .nav-bar .nav-list .hamburger"
-);
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+function toggleModal() { modal.classList.toggle("show-modal");}
+function windowOnClick(event) {if (event.target === modal) { toggleModal();}}
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const menu_item = document.querySelectorAll(
-  ".header .nav-bar .nav-list ul li a"
-);
+const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
 const header = document.querySelector(".header.container");
 
 hamburger.addEventListener("click", () => {
@@ -27,20 +32,3 @@ menu_item.forEach((item) => {
     mobile_menu.classList.toggle("active");
   });
 });
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
-
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
-
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
