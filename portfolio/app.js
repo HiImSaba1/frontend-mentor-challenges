7b28615c87d8390de32b1b8def5b282094848297
Pavlos Saba <pavlossaba@gmail.com>
2020-06-11 18:40:12 +0300

    Fixed some stuff

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,15 +1,26 @@
 const modal = document.querySelector(".modal");
 const trigger = document.querySelector(".trigger");
 const closeButton = document.querySelector(".close-button");
-function toggleModal() { modal.classList.toggle("show-modal");}
-function windowOnClick(event) {if (event.target === modal) { toggleModal();}}
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
+}
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
 window.addEventListener("click", windowOnClick);
 
-const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
 const header = document.querySelector(".header.container");
 
 hamburger.addEventListener("click", () => {
